f389e12a42fc328cec17e5b584541d3eee50a878
Keith Bowes <zooplah@gmail.com>
2015-04-07 20:30:18 -0400

    Finished the German and Spanish translations

--- locales/de_DE/_global.php
+++ locales/de_DE/_global.php
@@ -7,17 +7,23 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 
 $trans['de_DE'] = array(
 '__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
-'' => "Project-Id-Version: WP to Diaspora*\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2015-04-04 00:44-0400\nPO-Revision-Date: 2015-04-03 20:08-0400\nLast-Translator: Katrin Leinweber\nLanguage-Team: \nLanguage: de\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nX-Generator: POEditor.com\n",
+'' => "Project-Id-Version: WP to Diaspora*\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2015-04-04 00:44-0400\nPO-Revision-Date: 2015-04-07 20:12-0400\nLast-Translator: Katrin Leinweber\nLanguage-Team: \nLanguage: de\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nX-Generator: POEditor.com\n",
 'Diaspora* Plugin for b2evolution' => 'Diaspora*-Plugin für b2evolution',
 'Post to your Diaspora* account when you post to your blog' => 'Einen Diaspora*-Eintrag erstellen, wenn Sie Einträge in Ihrem Blog machen',
 'Posts to your Diaspora* account to update %s with details of your blog post.' => 'Benachrichtigt Ihr Diaspora*-Konto, um %s mit Details zu Ihrem Blog-Eintrag zu aktualisieren.',
 'Update your Diaspora* account with details about the new post.' => 'Aktualisieren Sie Ihr Diaspora*-Konto mit Details über den neuen Eintrag.',
 'The plugin needs a non-empty code.' => 'Das Plugin erfordert einen nicht leeren Code.',
+'This plugin requires PHP 5.2.0 or higher.' => 'Das Plugin erfordert PHP 5.2.0 oder höher.',
 'This plugin requires the PHP cURL extension.' => 'Das Plugin erfordert die PHP cURL-Erweiterung.',
 'Successfully logged in to <a href="%1$s">%1$s</a> as %2$s.' => 'Mit Erfolg hat der Benutzer „%2$s” ins Pod <a href="%1$s">%1$s</a> eingeloggt.',
 'Diaspora* account status' => 'Diaspora* Kontostato',
 'Diaspora* Pod' => 'Diaspora* Pod',
+'Username' => 'Benutzername',
+'Password' => 'Passwort',
 'Aspects' => 'Aspekte',
+'Just posted $title$ $url$ #b2p' => 'Hat gerade $title$ $url$ #b2p eingetragen',
+'Message format' => 'Nachrichtenformat',
+'$title$, $excerpt$ and $url$ will be replaced appropriately.' => '$title$, $excerpt$ und $url$ werden entsprechend ersetzt.',
 'Failed to initialise connection to pod "%s".' => 'Scheitert initialisieren Verbindung zu Pod "%s".',
 'Not logged in.' => 'Nicht eingeloggt.',
 'Couldn\'t log in with the credentials specified.' => 'Konnte nicht mit den angegebenen Anmeldeinformationen einloggen.',
@@ -25,6 +31,7 @@ $trans['de_DE'] = array(
 'Unknown error occurred.' => 'Unbekannter Fehler fand statt.',
 'Error loading aspects.' => 'Fehler laden Aspekte.',
 'Error loading services.' => 'Fehler laden Bedienen.',
+'Public' => 'Öffentlich',
 
 );
 ?>
